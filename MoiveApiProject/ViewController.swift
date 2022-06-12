9cbfcf3fa3e7daa88baee03b0ca0b8f96f8073f5
Kyle <dpkyle97@gmail.com>
2022-06-12 14:32:14 +0900

    6.12
    초기 프로젝트 세팅

---
+++ MoiveApiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoiveApiProject
+//
+//  Created by 강창혁 on 2022/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
